ee6af248bb442e271fe02e043ea471340550afb3
John Goff <gygn.johngoff@gmail.com>
2018-03-25 13:33:45 -0400

    change code slightly so we pass a map instead of a list of args, lets us pass in any order and annotate variables

--- lib/subroutine/level_map.ex
+++ lib/subroutine/level_map.ex
@@ -7,18 +7,17 @@ defmodule LevelMap do
 
   defp floor_cell(x, y), do: %Cell{ type: "FLOOR", x: x, y: y }
 
-  defp genrow(list, x, y, size) when x > size do
+  defp genrow(list, %{x: x, y: y, max: max}) when x > max do
     list |> Enum.reverse
   end
 
-  defp genrow(list, x, y, size) do
-    [floor_cell(x, y) | list]
-    |> genrow(x + 1, y, size)
+  defp genrow(list, %{x: x, y: y, max: max}) do
+    [floor_cell(x, y) | list] |> genrow(%{x: x + 1, y: y, max: max})
   end
 
-  defp gencol(col) do
-    0..col
-    |> Enum.map(fn(x) -> genrow([], 0, x, col) end)
+  defp gencol(max) do
+    0..max
+    |> Enum.map(fn(y) -> genrow([], %{y: y, x: 0, max: max}) end)
     |> List.flatten
   end
 end
