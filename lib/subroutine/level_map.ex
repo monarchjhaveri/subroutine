b26f9ef24a8b7a959fe117a3f8ef928e3f086ee1
John Goff <gygn.johngoff@gmail.com>
2018-03-25 15:48:29 -0400

    begin adding walls

--- lib/subroutine/level_map.ex
+++ lib/subroutine/level_map.ex
@@ -1,18 +1,25 @@
 defmodule LevelMap do
-  @room_size 2
+  @room_size 20
+  def default_size, do: @room_size
 
   def gen(size \\ @room_size) do
     gencol size
   end
 
   defp floor_cell(x, y), do: %Cell{ type: "FLOOR", x: x, y: y }
+  defp wall_cell(x, y), do: %Cell{ type: "WALL", x: x, y: y }
 
-  defp genrow(list, %{x: x, y: y, max: max}) when x > max do
+  defp new_cell(%{x: x, y: y, max: max}) do
+    if x >= max || y >= max, do: wall_cell(x, y), else: floor_cell(x, y)
+  end
+
+  defp genrow(list, %{x: x, max: max}) when x > max do
     list |> Enum.reverse
   end
 
-  defp genrow(list, %{x: x, y: y, max: max}) do
-    [floor_cell(x, y) | list] |> genrow(%{x: x + 1, y: y, max: max})
+  defp genrow(list, opts) do
+    %{x: x} = opts
+    [new_cell(opts) | list] |> genrow(%{opts | x: x + 1})
   end
 
   defp gencol(max) do
