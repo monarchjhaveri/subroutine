3974a2a22b94072bc66de485d4cb02d9dbbb4d80
John Goff <gygn.johngoff@gmail.com>
2018-03-25 13:23:35 -0400

    generate rooms of different sizes properly

--- lib/subroutine/level_map.ex
+++ lib/subroutine/level_map.ex
@@ -7,18 +7,18 @@ defmodule LevelMap do
 
   defp floor_cell(x, y), do: %Cell{ type: "FLOOR", x: x, y: y }
 
-  defp genrow(list, x, y) when x > @room_size do
+  defp genrow(list, x, y, size) when x > size do
     list |> Enum.reverse
   end
 
-  defp genrow(list, x, y) do
+  defp genrow(list, x, y, size) do
     [floor_cell(x, y) | list]
-    |> genrow(x + 1, y)
+    |> genrow(x + 1, y, size)
   end
 
   defp gencol(col) do
     0..col
-    |> Enum.map(fn(x) -> genrow([], 0, x) end)
+    |> Enum.map(fn(x) -> genrow([], 0, x, col) end)
     |> List.flatten
   end
 end
