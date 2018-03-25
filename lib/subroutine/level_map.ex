7aad6835929cd11ffdf290e1a4dd54dc7b9a3103
John Goff <gygn.johngoff@gmail.com>
2018-03-25 12:04:14 -0400

    generate rows and columns with recursion rather than copy/paste

--- lib/subroutine/level_map.ex
+++ lib/subroutine/level_map.ex
@@ -1,15 +1,24 @@
 defmodule LevelMap do
+  @room_size 2
+
   def gen do
-    [
-      %Cell{ type: "FLOOR", x: 0, y: 0},
-      %Cell{ type: "FLOOR", x: 1, y: 0},
-      %Cell{ type: "FLOOR", x: 2, y: 0},
-      %Cell{ type: "FLOOR", x: 0, y: 1},
-      %Cell{ type: "FLOOR", x: 1, y: 1},
-      %Cell{ type: "FLOOR", x: 2, y: 1},
-      %Cell{ type: "FLOOR", x: 0, y: 2},
-      %Cell{ type: "FLOOR", x: 1, y: 2},
-      %Cell{ type: "FLOOR", x: 2, y: 2}
-    ]    
+    gencol @room_size
+  end
+
+  defp floor_cell(x, y), do: %Cell{ type: "FLOOR", x: x, y: y }
+
+  defp genrow(list, x, y) when x > @room_size do
+    list |> Enum.reverse
+  end
+
+  defp genrow(list, x, y) do
+    [floor_cell(x, y) | list]
+    |> genrow(x + 1, y)
+  end
+
+  defp gencol(col) do
+    0..col
+    |> Enum.map(fn(x) -> genrow([], 0, x) end)
+    |> List.flatten
   end
 end
