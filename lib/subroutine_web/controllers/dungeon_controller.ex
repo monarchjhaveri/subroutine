5b5b75fcc41495a2c55d898f9b8355935047433b
John Goff <john.g@hiredhippo.ai>
2018-03-31 10:27:27 -0400

    fix function guard

--- lib/subroutine_web/controllers/dungeon_controller.ex
+++ lib/subroutine_web/controllers/dungeon_controller.ex
@@ -1,14 +1,14 @@
 defmodule SubroutineWeb.DungeonController do
   use SubroutineWeb, :controller
 
+  def index(%{request_path: req} = conn, _params) when req == "/api" do
+    json(conn, build_response(20))
+  end
+
   def index(conn, _params) do
-    if conn.request_path == "/api" do
-      json(conn, build_response(20))
-    else
-      conn
-      |> put_resp_header("content-type", "text/html; charset=utf-8")
-      |> Plug.Conn.send_file(200, "assets/static/index.html")
-    end
+    conn
+    |> put_resp_header("content-type", "text/html; charset=utf-8")
+    |> Plug.Conn.send_file(200, "assets/static/index.html")
   end
 
   defp build_response(room_size) do
