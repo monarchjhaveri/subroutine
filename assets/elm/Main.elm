ab0ea353aa58ad51d28ad76aa5a3a3b922085dbe
John Goff <john.g@hiredhippo.ai>
2018-04-01 19:25:59 -0400

    add keyboard message, model and related functions

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -6,6 +6,7 @@ import Http
 import Json.Decode exposing (..)
 import Json.Decode.Pipeline exposing (decode, required)
 import List.Extra
+import Keyboard.Extra exposing (Key)
 
 
 -- MSG
@@ -14,6 +15,7 @@ import List.Extra
 type Msg
     = GetState
     | NewState (Result Http.Error GameData)
+    | KeyPress Keyboard.Extra.Msg
 
 
 
@@ -23,6 +25,7 @@ type Msg
 type alias Model =
     { data : GameData
     , err : Error
+    , key : List Key
     }
 
 
@@ -89,6 +92,27 @@ update msg model =
 
         NewState (Err _) ->
             ( { model | err = FetchFail }, Cmd.none )
+
+        KeyPress k ->
+            let
+                keys =
+                    Keyboard.Extra.update k model.key
+
+                pressed =
+                    case List.head keys of
+                        Nothing ->
+                            -- value doesn't matter, not actually used
+                            Keyboard.Extra.Cancel
+
+                        Just key ->
+                            key
+
+                action =
+                    case pressed of
+                        _ ->
+                            Cmd.none
+            in
+                ( { model | key = keys }, action )
 
 
 getServerState : Cmd Msg
@@ -208,7 +232,7 @@ subscriptions model =
 
 init : ( Model, Cmd Msg )
 init =
-    ( (Model (GameData [] 0 (PlayerLocation 0 0)) None), getServerState )
+    ( (Model (GameData [] 0 (PlayerLocation 0 0)) None []), getServerState )
 
 
 main : Program Never Model Msg
