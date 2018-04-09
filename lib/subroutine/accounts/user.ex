1c3ebff88185c40e8998ce5b5c8544b21c1105bb
John Goff <john.g@hiredhippo.ai>
2018-04-09 10:09:28 -0400

    registration changeset

--- lib/subroutine/accounts/user.ex
+++ lib/subroutine/accounts/user.ex
@@ -1,11 +1,13 @@
 defmodule Subroutine.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias Comeonin.Bcrypt
 
   schema "users" do
     field :password_hash, :string
     field :username, :string
+    field :seed, :string
+    field :moves, {:array, :string}
 
     timestamps()
   end
@@ -15,5 +17,25 @@ defmodule Subroutine.Accounts.User do
     user
     |> cast(attrs, [:username, :password_hash])
     |> validate_required([:username, :password_hash])
+  end
+
+  def registration_changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:username, :password])
+    |> validate_required([:username, :password])
+    |> validate_length(:username, min: 3, max: 20)
+    |> validate_length(:password, min: 8)
+    |> unique_constraint(:username)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: passwd}} ->
+        put_change(changeset, :password_hash, Bcrypt.hashpwsalt(passwd))
+
+      _ ->
+        changeset
+    end
   end
 end
